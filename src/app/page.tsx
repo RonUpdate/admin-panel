00a28d6a91e1207336c9ff0904264724bf5e0fab
unknown <email.com>
2025-04-01 11:34:44 +0300

    fix: add use client to redirect page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { redirect } from "next/navigation"
 
 export default function Home() {
-  redirect("/admin/products/new")
+    redirect("/admin/products/new")
+    return null
 }
